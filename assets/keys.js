81b01416940973c1b6fab905f216769639d63f4f
CLRB17 <cynthia@brown-house.net>
2017-04-30 13:02:02 -0400

    initial commit

---
+++ assets/keys.js
@@ -0,0 +1,8 @@
+console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: '5KORJKZQbUmBNkf5Ni1REKVXU',
+  consumer_secret: 'y65qef0HYmNqqPQluiRqwGRpIPE4C22fZVdFpbZyTzQfDhV1Xe',
+  access_token_key: '858724314971086849-wLwA80csn8x2F4ivo434RLJLNWGfsG3',
+  access_token_secret: 'wbPd2LjF722KQXNwyDff2ht0NkfDqiybicKYy01GxIIBv',
+}
